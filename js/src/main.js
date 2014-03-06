61b628d025401849d1b7b57a73cf90078abc07d6
Eric Magnuson <eric@ericmagnuson.me>
2014-03-06 01:20:53 -0700

    Swapping words with CSS animation; snapped pixels with email icon

--- js/src/main.js
+++ js/src/main.js
@@ -12,7 +12,7 @@ require.config({
 	}
 });
 
-require(['jquery'], function($, Modernizr){
+require(['jquery'], function($){
 	// In hoc signo vinces.
 	$(document).ready(function(){
 
@@ -31,22 +31,57 @@ require(['jquery'], function($, Modernizr){
 
 		size_areas();
 
+
 		(function(){
 
 			var words = [
 				// &#8209; is a non-breaking hyphen
-				'beautiful',
-				'pixel&#8209;perfect',
-				'mobile',
-				'well&#8209;built',
-				'responsive'
+				'beautiful&nbsp;',
+				'pixel&#8209;perfect&nbsp;',
+				'mobile&nbsp;',
+				'well&#8209;built&nbsp;',
+				'responsive&nbsp;'
 			], i = 0;
 
-			setInterval(function(){
-				$('#rotate-word').fadeOut(function(){
-					$(this).html(words[i=(i+1)%words.length]).fadeIn(300);
+			var swap_word = function swap_word(){
+				// 1) Fade out
+				// 2) Resize
+				// 3) Fade in next word
+				// 4) END (and wait the setInterval duration)
+
+				var $word_span    = $('#rotate-word'),
+					next_word     = words[i=(i+1)%words.length],
+					$next_word    = $('<strong/>').html(next_word),
+					current_width = $word_span.outerWidth(),
+					next_width    = 0;
+
+				// Append the next word in a hidden area
+				// just to measure the width we'll animate to.
+				$next_word.css({
+					position: 'absolute',
+					right: '100%'
+				}).appendTo($word_span.parent());
+				next_width = $next_word.outerWidth();
+				$next_word.remove();
+
+				// Explicitly set the current width so that it registers for the
+				// CSS transition.  Then, remove the old fadeInDown animate class
+				// (if it's there), and add the fadeOutDown class.
+				$word_span.width(current_width).removeClass('fadeInDown').addClass('fadeOutDown');
+
+				// Once that animation is done, set the new target width, add the new
+				// word, remove the old animate class, and finally add the new fadeInDown class.
+				// Note: Delays and durations for the fadeInDown and fadeOutDown are set in CSS.
+				$word_span.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+					$word_span.width(next_width).html(next_word).removeClass('fadeOutDown').addClass('fadeInDown');
 				});
-			}, 5000);
+
+				// Start the timer again.
+				setTimeout(swap_word(), 5000);
+			};
+
+			// Init word swap
+			setTimeout(swap_word(), 1000);
 
 		})();
 
