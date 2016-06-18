6ba18071418a02d0bd216ed6ab8778184a0a6216
Eric Magnuson <eric@ericmagnuson.me>
2016-06-18 15:10:08 -0600

    Updating gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
 					paths: ['css'],
 					compress: true,
 					cleancss: true,
-					report: 'gzip'
+					report: 'min'
 				},
 				files: [{
 					expand: true,
